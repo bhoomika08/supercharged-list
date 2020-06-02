17ee08d4645bddfdc0eee0831d54c5a6ddbe2e55
Bhoomika <bhoomika@vinsol.com>
2020-06-02 20:00:02 +0530

    Updates

---
+++ precache-manifest.e99c6aa612f92312647a8066084a3c35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c13dc0c6ff82854799b097b2affeb7f7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "66d176f3bb7f9d6a9709",
+    "url": "./static/css/main.9e018e87.chunk.css"
+  },
+  {
+    "revision": "d5a91b10d82ee94691b9",
+    "url": "./static/js/2.fc029293.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "./static/js/2.fc029293.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66d176f3bb7f9d6a9709",
+    "url": "./static/js/main.50708a76.chunk.js"
+  },
+  {
+    "revision": "8ec0bb23fc1753440989",
+    "url": "./static/js/runtime-main.caf19b78.js"
+  }
+]);
