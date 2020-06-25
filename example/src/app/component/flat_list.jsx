5a22e383d739854b26cbcbca26307b13131bad99
Bhoomika <bhoomika@vinsol.com>
2020-06-25 16:17:26 +0530

    CR-changes

--- example/src/app/component/flat_list.jsx
+++ example/src/app/component/flat_list.jsx
@@ -15,20 +15,23 @@ class FlatList extends React.Component {
     this.state = {
       items: []
     };
-    this.lastItemRef = React.createRef();
+    this.getLastElementHeight = this.getLastElementHeight.bind(this);
+    this.getKey = this.getKey.bind(this);
     this.loadMoreItems = this.loadMoreItems.bind(this);
-    this.scrollToElement = this.scrollToElement.bind(this);
+    this.scrollToItem = this.scrollToItem.bind(this);
   }
 
   componentDidMount() {
-    const { data, batchCount, isVirtual, isScroll, scrollParent } = this.props;
+    const { data, batchCount, autoLoad, loadOnScroll, scrollParent } = this.props;
     const { items } = this.state;
 
-    if (isVirtual) {
-      this.setState({
-        items: data.slice(0, batchCount)
-      });
-    } else if (isScroll) {
+    if (autoLoad) {
+      if (data.length > 0) {
+        this.setState({
+          items: data.slice(0, batchCount)
+        });
+      }
+    } else if (loadOnScroll) {
       if (data.length > 0 && items.length == 0) {
         this.setState({
           items: data.slice(0, batchCount)
@@ -37,7 +40,7 @@ class FlatList extends React.Component {
       const parent = scrollParent ? document.querySelector(scrollParent) : window;
       const parentHeight = scrollParent ? parent.clientHeight : window.innerHeight;
       parent.addEventListener("scroll", () => {
-        if (this.lastItemRef.current.getBoundingClientRect().bottom - 300 <= parentHeight) {
+        if (this.getLastElementHeight() - 300 <= parentHeight) {
           this.loadMoreItems();
         }
       });
@@ -45,15 +48,20 @@ class FlatList extends React.Component {
   }
 
   componentDidUpdate({ data: oldData }) {
-    const { data, batchCount, isVirtual, isScroll, itemToScrollPosition } = this.props;
+    const { data, batchCount, autoLoad, loadOnScroll, positionToScroll } = this.props;
     const { items } = this.state;
-    if (isVirtual) {
+
+    //---------- To Load Items Automatically -----------//
+    if (autoLoad) {
       if (oldData.length == data.length && itemChecker({ data: oldData }, { data })) {
         this.loadMoreItems();
       } else {
         this.setState({ items: [] });
       }
-    } else if (isScroll) {
+    }
+
+    //---------- To Load first batch of Items if items length is 0 before scrolling ---------//
+    else if (loadOnScroll) {
       if (oldData.length == data.length && itemChecker({ data: oldData }, { data })) {
         if (items.length == 0)
           this.setState({
@@ -63,11 +71,13 @@ class FlatList extends React.Component {
         this.setState({ items: [] });
       }
     }
-    if (itemToScrollPosition >= 0 && itemToScrollPosition != null) {
-      if (itemToScrollPosition > items.length) {
+
+    //------ To Scroll to a particular Item -----------//
+    if (positionToScroll != null && positionToScroll >= 0) {
+      if (positionToScroll > items.length) {
         this.loadMoreItems();
       } else {
-        this.scrollToElement(itemToScrollPosition);
+        this.scrollToItem(data[positionToScroll], positionToScroll);
       }
     }
   }
@@ -76,13 +86,21 @@ class FlatList extends React.Component {
     window.removeEventListener("scroll", this.handleOnScroll);
   }
 
+  getLastElementHeight() {
+    const { items } = this.state;
+    return this.refs[`separator-ref-${this.getKey(items[items.length - 1])}`].getBoundingClientRect().bottom;
+  }
+
+  getKey(item) {
+    const { itemKey } = this.props;
+    return typeof item == "object" ? item[itemKey] : item;
+  }
+
   loadMoreItems() {
     const { data, batchCount } = this.props;
     const { items } = this.state;
     clearTimeout(this.timeoutId);
-    const hasMore =
-      items.length + batchCount <= data.length ||
-      (data.length - items.length <= batchCount && data.length !== items.length);
+    const hasMore = data.length - items.length > 0;
     if (hasMore) {
       this.timeoutId = setTimeout(() => {
         this.setState(prevState => ({
@@ -92,26 +110,24 @@ class FlatList extends React.Component {
     }
   }
 
-  scrollToElement(itemPosition) {
-    const { onScrollEnd } = this.props;
-    const { items } = this.state;
-    const referenceElement = itemPosition == items.length - 1 ? this.lastItemRef.current : this.refs[`Ref-${itemPosition}`];
+  scrollToItem(item) {
+    const { onScrollToElementEnd } = this.props;
+    const referenceElement = this.refs[`separator-ref-${this.getKey(item)}`];
     clearTimeout(this.scrollEnd);
     referenceElement.scrollIntoView({
       behavior: "smooth",
       block: "center"
     });
-    this.scrollEnd = setTimeout(() => onScrollEnd(), 3000);
+    this.scrollEnd = setTimeout(onScrollToElementEnd, 3000);
   }
 
   render() {
-    const { data, renderItem, itemKey, isVirtual, isScroll, ListItemType } = this.props;
+    const { data, renderItem, autoLoad, loadOnScroll, SeparatorType } = this.props;
     const { items } = this.state;
-    const lastIndex = items.length - 1;
-    return (isVirtual || isScroll ? items : data).map((item, idx) => (
-      <React.Fragment key={`List-${typeof item == "object" ? item[itemKey] : item}`}>
+    return (autoLoad || loadOnScroll ? items : data).map((item, idx) => (
+      <React.Fragment key={`List-${this.getKey(item)}`}>
         <ItemRenderer item={item} renderItem={renderItem} />
-        <ListItemType ref={lastIndex == idx ? this.lastItemRef : `Ref-${idx}`}></ListItemType>
+        <SeparatorType ref={`separator-ref-${this.getKey(item)}`} />
       </React.Fragment>
     ));
   }
@@ -120,11 +136,11 @@ class FlatList extends React.Component {
 FlatList.defaultProps = {
   batchCount: 50,
   itemKey: "id",
-  isVirtual: false,
-  isScroll: false,
-  ListItemType: "span",
-  itemToScrollPosition: null,
-  onScrollEnd: () => {}
+  autoLoad: false,
+  loadOnScroll: false,
+  SeparatorType: "span",
+  positionToScroll: null,
+  onScrollToElementEnd: () => {}
 };
 
 export default FlatList;
